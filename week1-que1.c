342003c4e3f5598c3e67c29ae0f1e689ecae8ef5
IshitaSatpathy <76519947+IshitaSatpathy@users.noreply.github.com>
2021-02-07 21:25:32 +0530

    week1-que1

--- week1-que1.c
+++ week1-que1.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2,sum;
+    int num1,num2,sum,mul,div,min;
     printf("Enter two numbers:");
     scanf("%d %d", &num1,&num2);
     sum= num1+num2;
+    mul= num1*num2;
+    div= num1/num2;
+    min= num1-num2;
     printf("%d + %d = %d", num1, num2, sum);
+    printf("%d - %d = %d", num1, num2, min);
+    printf("%d x %d = %d", num1, num2, mul);
+    printf("%d / %d = %d", num1, num2, div);
     return 0;
 }
 
